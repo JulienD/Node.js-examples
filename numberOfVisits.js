81694ed91f42ba4fc12352c4ba428794564a2c1f
Julien <dubreuil.julien@gmail.com>
2012-03-08 14:17:12 -0800

    Removing the part to try to catch disconnection from the client

--- numberOfVisits.js
+++ numberOfVisits.js
@@ -5,8 +5,6 @@ var http = require('http');
 var visits = 0;
 // Number of visitors.
 var visitors = 0;
-// Number of current.
-var currentVisitors = 0;
 
 // Setup an http server.
 var server = http.createServer(function (request, response) {
@@ -14,22 +12,14 @@ var server = http.createServer(function (request, response) {
   	response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello!\n');
     response.write('We have had ' + visits + ' visits for ' + visitors + ' visitors today\n');
-    response.write('Currently we have ' + currentVisitors + ' visitors\n');
     response.end();
 });
 
 server.on('connection', function (socket) { 
 	visitors++;
-	currentVisitors++;
 	// Display each connection to the server in the console.
 	console.log('New connection');
 });
-
-server.on('close', function (errno) { 
-	currentVisitors--;
-	console.log('Connection closed');	
-});
-
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
